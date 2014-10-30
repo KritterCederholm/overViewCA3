6f3cd0b3fa44e9d4b193ea111f23cb6c87bf9e99
Skumfidusen1992 <Skumfidusen1992@gmail.com>
2014-10-30 09:44:40 +0100

    added route for showall orders in app

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var showall = require('./routes/ShowAll');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/ShowAll', showall);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
