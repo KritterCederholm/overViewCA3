90e7e261740e0ff12046e5029b89618d8d6449f8
TanjaCe <minpaloma@hotmail.com>
2014-10-31 13:52:35 +0100

    Standart commit

--- app.js
+++ app.js
@@ -1,52 +1,16 @@
 var express = require('express');
 var path = require('path');
-var mongoose = require('mongoose');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var fs = require('fs');
-
-var model = require('./database/model');
-var mongo = require('./database/mongo');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var showall = require('./routes/ShowAll');
+var ShowAll = require('./routes/ShowAll');
+var orderDetails = require('./routes/orderDetails')
 
 var app = express();
-
-//var db = mongoose.connect("mongodb://viewer:testviewer@ds049150.mongolab.com:49150/orderview");
-
-mongo.connect();
-
-model.ProductModel.find(function(err, orderdetails){
-/*
-    fs.writeFile("./public/OrderHistory", orderdetails, function(err) {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log("The file was saved!");
-        }
-    });
-    */
-
-    console.log(orderdetails);
-    mongo.close();
-})
-
-//var person = new model.PersonModel({name:'Naja', age:'27'});
-
-/*
- person.save(function(err,persons) {
- if(err){
- console.log(err);
- } else {
- console.log("Person saved: " + person);
- }
- //mongo.close();
- });
- */
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -62,12 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/ShowAll', showall);
+app.use('/ShowAll', ShowAll);
+app.use('/OrderDetails', orderDetails)
 
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
