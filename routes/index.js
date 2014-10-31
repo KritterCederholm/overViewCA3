fa24ada4cf017b39e68ad4a26118de5f9930670d
blofmark <bloefmark@yahoo.dk>
2014-10-31 13:15:46 +0100

    FrontEnd:  Made the index page and added some styling to the webpages -- Bettina

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'MongoDB Viewer' });
+  res.render('index', { title: 'CA 3 MEB Application' });
 });
 
 module.exports = router;
