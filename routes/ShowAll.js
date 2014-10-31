df51db366528e5d87501a8b8c39ba905611cbe16
Skumfidusen1992 <Skumfidusen1992@gmail.com>
2014-10-31 09:50:30 +0100

    fixed route for showall orders in app to work with mongoose instead of monojs

--- routes/ShowAll.js
+++ routes/ShowAll.js
@@ -3,25 +3,22 @@
  */
 var express = require('express');
 var router = express.Router();
-var collections = ('orders');
-var db = require('mongojs').connect("mongodb://viewer:testviewer@ds049150.mongolab.com:49150/orderview", collections);
-
+var model = require('../database/model');
 var orderArray = [];
 
-db.orders.find(function (err, orders){
+model.OrderModel.find().sort({'orderDate':1})
+    .exec(function (err, orders) {
 
-    orders.forEach(function(order){
-        orderArray.push(order);
+        orders.forEach(function (order) {
+            orderArray.push(order);
+        });
     });
-});
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('showall',
+    res.render('showAll',
         {
-           title: 'List of all orders',
-           orderArray: orderArray
+            title: 'Here are all orders',
+            orderArray: orderArray
         });
 });
-
-module.exports = router;
